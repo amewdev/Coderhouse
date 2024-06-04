cd961ac7f2aa790d3f40bb3166e52dce7d35f77f
amewdev <amecodea@gmail.com>
2024-06-04 13:43:38 -0300

    Add files via upload
    I have second read the assignment. I have implemented status as 'true' by default.

--- backend1/1/src/routes/products.router.js
+++ backend1/1/src/routes/products.router.js
@@ -28,37 +28,27 @@ router.get('/:pid', async (req,res) => {
 });
 
 router.post('/', async (req, res) => {
-    let { title, description, code, price, status, stock, category } = req.body;
+    let { title, description, code, price, stock, category } = req.body;
 
     price = Number(price);
-    status = status.toLowerCase();
     stock = Number(stock);
 
     try {
-        if (!title || !description || !code || !price || !status || !stock || !category) {
+        if (!title || !description || !code || !price || !stock || !category) {
             res.status(400).send({ state: "error", error: "A required field is missing" });
             return;
         };
         //validation of every field
-        switch (status) {
-            case 'true' :
-                status = true;
-                break;
-            case 'false' :
-                status = false;
-                break;
-        };
 
         //if (typeof title !== 'string') {throw new Error("Title must be String")};
         //if (typeof description !== 'string') {throw new Error("Description must be String")};
         //if (typeof code !== 'string') {throw new Error("Code must be String")};
         if (isNaN(price)) {throw new Error("Price must be Number")};
-        if (typeof status !== 'boolean') {throw new Error("Status must be Boolean")};
         if (isNaN(stock)) {throw new Error("Stock must be Number")};
         //if (typeof category !== 'string') {throw new Error("Category must be String")};
         //end of validation
 
-        await productManager.postProduct(title, description, code, price, status, stock, category);
+        await productManager.postProduct(title, description, code, price, stock, category);
         res.status(201).send({ state: "success", message: "Product added successfully" });
     } catch (error) {
         res.status(500).send({ state: "error", message: error.message });
@@ -70,28 +60,24 @@ router.put('/:pid', async (req, res) => {
     let { property, newValue } = req.body;
 
     pid = Number(pid);
+    if (isNaN(pid)) {throw new Error ("Id must be Number")};
+
     property = property.toLowerCase().replace(/\s+/g, '');
     try {
         if (!property || !newValue) {throw new Error("A required field is missing")};
 
         if (property === 'status') {
-            switch (newValue.toLowerCase().replace(/\s+/g, '')) {
-                case 'true':
-                    newValue = true;
-                    break;
-                case 'false':
-                    newValue = false;
-                    break;
+            switch (newValue.replace(/\s+/g, '').toLowerCase()) {
+                case 'true': newValue = true; break;
+                case 'false': newValue = false; break;
+                default: throw new Error("Status must be Boolean");
             };
-        };
-
-        if (isNaN(pid)) {throw new Error ("Id must be Number")};
-
-        switch (property) {
-            case 'id' : throw new Error ("Id should not be changed");
-            case 'price' : if (isNaN(Number(newValue))) {throw new Error ("Price must be Number")};
-            case 'status' : if (typeof newValue !== 'boolean') {throw new Error ("Status must be Boolean")};
-            case 'stock' : if (isNaN(Number(newValue))) {throw new Error ("Stock must be Number")};
+        } else {
+            switch (property) {
+                case 'id' : throw new Error ("Id should not be changed");
+                case 'price' : if (isNaN(Number(newValue))) {throw new Error ("Price must be Number")}; break;
+                case 'stock' : if (isNaN(Number(newValue))) {throw new Error ("Stock must be Number")};
+            };
         };
 
         if (property === 'price' || property === 'stock') {
